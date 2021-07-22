ac4b8577cfe482e2ed1af542b8064258101ee2ca
杨圆建 <624508914@qq.com>
2021-07-22 18:28:03 +0800

    fix: 自定义错误

--- service/app/api/internal/logic/passport/login.go
+++ service/app/api/internal/logic/passport/login.go
@@ -2,8 +2,8 @@ package passport
 
 import (
 	"context"
-	"errors"
 	"tinkgo/service/app/api/internal/svc"
+	"tinkgo/service/pkg/errorx"
 )
 
 type LoginRequest struct{}
@@ -23,5 +23,5 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 }
 
 func (logic *LoginLogic) Login(req *LoginRequest) (*LoginResponse, error) {
-	return &LoginResponse{}, errors.New("dsfsdafsf")
+	return &LoginResponse{}, errorx.WithMsg("我也不知道你干嘛了")
 }
